a6d26770813537b2cfea2c03d511a7123d0eeff9
Seif Kamal <seifskm@gmail.com>
2016-11-15 14:59:27 +0000

    listen for return key press

--- scripts/rename.js
+++ scripts/rename.js
@@ -4,7 +4,7 @@
 
 function changeTitle() {
     var newTitle = document.getElementById('newTitle').value;
-    if(newTitle !== '') {
+    if (newTitle !== '') {
         chrome.tabs.executeScript(
             {
                 code: 'document.title="' + newTitle + '"'
@@ -19,3 +19,8 @@ chrome.tabs.getSelected(null, function (tab) {
 });
 
 document.getElementById('renameTab').addEventListener('click', changeTitle);
+document.getElementById('newTitle').addEventListener('keypress', function (event) {
+    if (event.keyCode == 13) {
+        changeTitle();
+    }
+});
