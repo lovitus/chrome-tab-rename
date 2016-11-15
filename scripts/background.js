09d5612e5db87d64d700dfb2340a9f08474af181
Seif Kamal <seifskm@gmail.com>
2016-11-15 12:36:06 +0000

    update scripts

--- scripts/background.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by seifkamal on 15/11/2016.
- */
-
--- scripts/rename.js
+++ scripts/rename.js
@@ -2,9 +2,12 @@
  * Created by seifkamal on 15/11/2016.
  */
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    console.log('Renaming');
-    chrome.tabs.executeScript({
-        code: 'document.title="Renamed"'
-    });
-});
+function changeTitle() {
+    chrome.tabs.executeScript(
+        {
+            code: 'document.title="Renamed"'
+        }
+    );
+}
+
+document.getElementById('renameTab').addEventListener('click', changeTitle);
