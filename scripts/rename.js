300d070c1fbe05fc7f85f38b175cb9b00d631c8a
Seif Kamal <seifskm@gmail.com>
2016-11-15 16:34:29 +0000

    use chrome.storage

--- scripts/rename.js
+++ scripts/rename.js
@@ -11,12 +11,26 @@ function changeTitle() {
             }
         );
     }
+    //save(newTitle);
     window.close();
 }
 
+var pageUrl;
 chrome.tabs.getSelected(null, function (tab) {
     document.getElementById('newTitle').value = tab.title;
+    pageUrl = tab.url;
 });
+
+function save(title) {
+    var data = {};
+    data[pageUrl] = title;
+    chrome.storage.local.set({'currentTitle': data}, function () {
+        //Settings saved
+    });
+}
+
+// chrome.storage.local.get(data, function(result){
+// });
 
 document.getElementById('renameTab').addEventListener('click', changeTitle);
 document.getElementById('newTitle').addEventListener('keypress', function (event) {
